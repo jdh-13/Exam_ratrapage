2863b0cac3303c998ab5d0b67e52a078c8b8489b
jerome De HARO <jeje13131313@hotmail.fr>
2019-09-18 13:53:05 +0200

    premierCommit

---
+++ exam_ex1.php
@@ -0,0 +1,36 @@
+<?php
+// exercice 1 exam php 
+//Jérôme De HARO 
+//l’algorithme affichant à l’écran la plus grande valeur de ce tableau.
+
+function LePlusGrandNombre($Tab)
+{
+	//index
+	$i;
+	//taille du tableau
+	$finI;
+	//le plus grand nombre
+	$GrandNomb = 0;
+	// le nombre courant 
+	$NombreCourant;
+
+	$finI= count($Tab);
+
+		for($i=0;i<$finI;$i++)
+		{
+			$NombreCourant=$Tab[$i];
+
+			if($i=0)
+			{
+				$NombreCourant=$GrandNomb;
+			}
+
+			if ($NombreCourant>=$GrandNomb) 
+			{
+				$NombreCourant=$GrandNomb;
+			}	
+		}
+
+		return $GrandNomb;
+		print_r($GrandNomb);
+}
---
+++ exam_ex2.php
@@ -0,0 +1,53 @@
+<?php
+// exercice 2 exam php 
+//Jérôme De HARO
+//Créer une classe « ArrayManager », qui reçoit en constructeur un tableau, possède une
+//propriété privée correspondant à ce tableau, un getter et un setter permettant d'y accéder
+//en lecture/écriture, et une méthode permettant de renvoyer le nombre le plus grand contenu dans ce tableau.
+/**
+ * 
+ */
+class arrayManager
+{
+	private $tableau;
+
+	//lecture
+	public function getTab()
+	{
+		return $this->tableau;
+	}
+
+	// modification
+	public function setTab($tableau)
+	{
+		$this->tableau=$tableau;
+	}
+
+	function __construct($tableau)
+	{
+		$this->tableau=$tableau;
+	}
+
+	public function plusGrandNombre()
+	{
+		$finI= count($this->tableau);
+
+		for($i=0;i<$finI;$i++)
+		{
+			$NombreCourant=$this->tableau[$i];
+
+			if($i=0)
+			{
+				$NombreCourant=$GrandNomb;
+			}
+
+			if ($NombreCourant>=$GrandNomb) 
+			{
+				$NombreCourant=$GrandNomb;
+			}	
+		}
+
+		return $GrandNomb;
+	}
+
+}
